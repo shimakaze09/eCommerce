6dd28c06725679973144a389ee3cc8e686c65b5a
John <yangtianzhuo970425@gmail.com>
2023-11-27 13:59:28 +1300

    Product preview modal components (Store)

--- ecommerce-store/components/ui/product-card.tsx
+++ ecommerce-store/components/ui/product-card.tsx
@@ -2,11 +2,13 @@
 
 import Image from "next/image";
 import { Expand, ShoppingCart } from "lucide-react";
+import { MouseEventHandler } from "react";
 import { useRouter } from "next/navigation";
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps {
     data: Product;
@@ -15,10 +17,17 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({
     data
 }) => {
+    const previewModal = usePreviewModal();
     const router = useRouter();
 
     const handleClick = () => {
         router.push(`/product/${data?.id}`)
+    }
+
+    const onPreview: MouseEventHandler<HTMLElement> = (event) => {
+        event.stopPropagation();
+
+        previewModal.onOpen(data);
     }
 
     return (
@@ -34,7 +43,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
                         <IconButton
-                            onClick={() => { }}
+                            onClick={onPreview}
                             icon={<Expand size={20} className="text-gray-600" />}
                         />
                         <IconButton
